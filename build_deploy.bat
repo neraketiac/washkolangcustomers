003ef678b8424a42aa4d24e646ebfba063c0a956
cen plang <karencaite@gmail.com>
2026-03-26 10:39:12 +0800

    new icons

--- build_deploy.bat
+++ build_deploy.bat
@@ -1,7 +1,23 @@
 @echo off
+
 for /f "tokens=1-5 delims=/ " %%a in ("%date%") do set d=%%c%%b%%a
 for /f "tokens=1-2 delims=:." %%a in ("%time: =0%") do set t=%%a%%b
 set BUILD_TIME=%d%-%t%
-echo Building with BUILD_TIME=%BUILD_TIME%
-flutter build web --release --dart-define=BUILD_TIME=%BUILD_TIME%
-firebase deploy --only hosting
+
+echo ====================================
+echo BUILD START %BUILD_TIME%
+echo ====================================
+
+echo Cleaning...
+call flutter clean
+
+echo Building...
+call flutter build web --release --dart-define=BUILD_TIME=%BUILD_TIME%
+
+echo Deploying...
+call firebase deploy --only hosting
+
+echo ====================================
+echo DONE
+echo ====================================
+pause
